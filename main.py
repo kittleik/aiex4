1cf69fbd7e802e6f80b62167fec32a8871094500
kittleik <sirkittelsen@gmail.com>
2016-04-28 01:06:06 +0200

    hei og hå

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def same_class(li):
     for data in li:
         if data[7] != unique:
             return False
-    return True
+    return unique
 
 def test_tree(tree,test_data):
     score = 0
@@ -95,20 +95,3 @@ a = decision_tree_learning(examples,random_importance)
 score = test_tree(a,test_data)
 print(a.feed_tree([0,1,0,0,1,1,0]))
 print (score)
-
-
-"""
-function DECISION-TREE-LEARNING(examples, attributes, parent examples) returns
-a tree
-    if examples is empty then return PLURALITY-VALUE(parent examples)
-    else if all examples have the same classification then return the classification
-    else if attributes is empty then return PLURALITY-VALUE(examples)
-    else
-        A ←argmaxa ∈ attributes IMPORTANCE(a, examples)
-        tree ← a new decision tree with root test A
-        for each value vk of A do
-            exs ← {e : e ∈examples and e.A = vk}
-            subtree ← DECISION-TREE-LEARNING(exs, attributes − A, examples)
-            add a branch to tree with label (A = vk) and subtree subtree
-        return tree
-"""
